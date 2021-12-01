f7157388ba2f0006ed2418b37a6e5d4c62e3ea59
PhanNgocLuan <luanphan110@gmail.com>
2021-12-01 11:34:55 +0700

    edit product

--- admin/formEditProduct.php
+++ admin/formEditProduct.php
@@ -1,4 +1,23 @@
+
 <?php include "header.php"?>
+<?php 
+  $product = new Product();
+  if(isset($_GET['id']))
+    {
+      $id = $_GET['id'];
+      $prodarr = $product->getProductById($id);
+      $name = $prodarr['name'];
+      $manu_name = $prodarr['manu_name'];
+      $manu_id = $prodarr['manu_id'];
+      $type_name = $prodarr['type_name'];
+      $type_id = $prodarr['type_id'];
+      $pro_image = $prodarr['pro_image'];
+      $created_at = $prodarr['created_at'];
+      $description = $prodarr['description'];
+      $price = $prodarr['price'];
+      $feature = $prodarr['feature'];
+    } 
+  ?>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -6,12 +25,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Product Add</h1>
+            <h1>Product Edit</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-              <li class="breadcrumb-item active">Product Add</li>
+              <li class="breadcrumb-item active">Product Edit</li>
             </ol>
           </div>
         </div>
@@ -19,7 +38,7 @@
     </section>
     <!-- Main content -->
     <section class="content">
-      <form action="addproduct.php" method="post" enctype="multipart/form-data">
+      <form action="./edit.php" method="post" enctype="multipart/form-data">
       <div class="row">
         <div class="col-md-12">
           <div class="card card-primary">
@@ -32,10 +51,11 @@
                 </button>
               </div>
             </div>
+           
             <div class="card-body">
               <div class="form-group">
                 <label for="name">Product Name</label>
-                <input type="text" name="name" class="form-control">
+                <input type="text" name="name" value="<?php echo $name?>" class="form-control">
               </div>
               <div class="form-group">
                 <label for="manu_id">Manufacture</label>
@@ -44,11 +64,16 @@
                     $manufacture = new Manufacture;
                     $getAllManu = $manufacture->getAllManu();
                     foreach($getAllManu as $value):
+                      if($value['manu_id'] == $manu_id):
                   ?>
-                  <option value=<?php echo $value['manu_id']?>>
+                  <option selected value=<?php echo $value['manu_id']?>>
                     <?php echo $value['manu_name'] ?>
                   </option>
-                  <?php endforeach ?>
+                  <?php else: ?>
+                    <option value=<?php echo $value['manu_id']?>>
+                      <?php echo $value['manu_name'] ?>
+                    </option>
+                  <?php endif; endforeach ?>
                 </select>
               </div>
               <div class="form-group">
@@ -57,30 +82,36 @@
                   <?php
                   $protype = new Protype;
                   $getAllProtype = $protype->getAllProtype();
-                  foreach($getAllProtype as $value): ?>
-                  <option value=<?php echo $value['type_id'] ?>>
+                  foreach($getAllProtype as $value):
+                  if($value['type_id'] == $type_id):
+                  ?>
+                  <option selected value=<?php echo $value['type_id'] ?>>
                     <?php echo $value['type_name'] ?>
                   </option>
-                  <?php endforeach ?>
+                  <?php else: ?>
+                    <option value=<?php echo $value['type_id'] ?>>
+                    <?php echo $value['type_name'] ?>
+                  </option>
+                  <?php endif; endforeach ?>
                 </select>
               </div>
               <div class="form-group">
                 <label for="price">Price</label>
-                <input type="text" name="price" class="form-control">
+                <input type="text" name="price" value="<?php echo $price ?>" class="form-control">
               </div>
               <div class="form-group">
                 <label for="description">Product Description</label>
-                <textarea name="desc" class="form-control" rows="4"></textarea>
+                <textarea name="desc" class="form-control" rows="4"><?php echo $description ?></textarea>
               </div>
               <div class="form-group">
                 <label for="feature">Feature</label>
-                <input type="number" name="feature" min="0" max="1" class="form-control">
+                <input type="number" name="feature" min="0" max="1" value="<?php echo $feature ?>" class="form-control">
               </div>
               <div class="form-group">
                 <label class="control-label">Choose an image :</label>
                 <div class="controls">
                   <input type="file" id='fileUpload' name="fileUpload" class="form-control">       
-                  <img src="" id='image' width='100' height='100'>              
+                  <img src="../img/<?php echo $pro_image ?>" id='image' width='100' height='100'>              
                 </div>
               </div>
             </div>
@@ -92,7 +123,9 @@
       </div>
       <div class="row">
         <div class="col-12">
-          <input name="submit" type="submit" value="Create new Product" class="btn btn-success float-right">
+          <input type="hidden" name="id" value="<?php echo $id?>">
+          <input type="hidden" name="action" value="product">
+          <input name="submit" type="submit" value="Edit Product" class="btn btn-success float-right">
         </div>
       </div>
       </form>
--- admin/models/product.php
+++ admin/models/product.php
@@ -21,4 +21,17 @@ class Product extends Db{
         $sql->bind_param("i",$id);
         return $sql->execute(); //return an object
     }
+    public function getProductById($id)
+    {
+        $sql = "SELECT * FROM products,protypes,manufactures WHERE products.manu_id = manufactures.manu_id AND products.type_id = protypes.type_id And products.id = $id";
+        $item = mysqli_query(self::$connection,$sql); 
+        $item1 = mysqli_fetch_assoc($item);
+        return $item1;
+    }
+    public function updateProduct($id,$name,$manu_id,$type_id,$price,$pro_image,$description,$feature)
+    {
+        $sql = self::$connection->prepare("UPDATE products 
+        SET name = '$name',manu_id = '$manu_id',type_id = '$type_id',price = '$price',pro_image = '$pro_image',description = '$description',feature = '$feature' WHERE id = '$id'");
+        return $sql->execute(); //return an object
+    }
 }
